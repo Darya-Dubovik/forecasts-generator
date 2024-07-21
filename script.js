87a2c8c2084f281904c36f58fcb2e70aa0a794a9
Darya-Dubovik <dashka_ivashenko@mail.ru>
2024-07-22 00:18:01 +0300

    Update script.js
    Исправила код (по комментарию наставника)

--- script.js
+++ script.js
@@ -1,35 +1,49 @@
 /* Генерация предсказания должна происходить при клике на кнопку «предсказать судьбу» */
 
 const button = document.querySelector('.forecast-btn');
-const forecasts = document.querySelector('.current-forecast h1');
-const text = document.querySelector('.current-forecast p');
-const elements = document.querySelector('#forecast-item');
-
-function generationNumber (min, max) {
-    return Math.floor(Math.random() * (max - min) + min);
-    
-    let predictionNumber = generationNumber; 
-    let predictionText = "";
-
-if (predictionNumber == 1) {
-    predictionText = "В скором времени тебе улыбнется удача!";
-} else if (predictionNumber == 2) {
-    predictionText = "Все будет лучше, чем сейчас!";
-} else if (predictionNumber == 3) {
-    predictionText = "Всё, что ни делается - всё к лучшему!";
-} else if (predictionNumber == 4) {
-    predictionText = "Жизнь готовит для тебя приятный сюрприз!";
-} else {
-    predictionText = "Скоро твой банковский счет заметно пополнится!";
-} 
-  }
-
-    elements.textContent = predictionText;
-    text.textContent = chance;
+const forecastText = document.querySelector('.current-forecast h1');
+const forecastPercentage = document.querySelector('.current-forecast p');
+const template = document.querySelector('#forecast-item');
+const forecasts = document.querySelector('.forecasts');
 
 button.addEventListener('click', function() {
-    const forecast = generationNumber (1, 5) (forecasts.textContent);
-    const chance = generationNumber (0, 100) (text.textContent);
+
+    function getRandom(min, max) {
+        return Math.floor (Math.random() * (max - min) + min);
+    }
+
+    function fillTemplate(forecast, percentage) {
+        let templateItem = template.content.cloneNode(true);
+        templateItem.querySelector('h3').textContent = forecast;
+        templateItem.querySelector('p').textContent = percentage;
+
+        return templateItem;
+    }
+
+    let textForecast;
+    const number = getRandom(1, 5);
+    const percent = getRandom (0, 100);
+
+        if (number == 1) {
+        textForecast = "В скором времени тебе улыбнется удача!"; 
+        } else if (number == 2) {
+        textForecast = "Все будет лучше, чем сейчас!";
+        } else if (number == 3) {
+        textForecast = "Всё, что ни делается - всё к лучшему!";
+        } else if (number == 4) {
+        textForecast = "Жизнь готовит для тебя приятный сюрприз!";
+        } else {
+        textForecast = "Скоро твой банковский счет заметно пополнится!";
+        }
+    
+    if (forecastText.textContent != "" && forecastPercentage.textContent != "") {
+        const templateItem = fillTemplate(forecastText.textContent, forecastPercentage.textContent);
+        forecasts.prepend(templateItem);
+    }
+
+    forecastText.textContent = textForecast;
+    forecastPercentage.textContent = "Вероятность " + percent + " % ";
+    
 })
 
 /* Заранее заготовь 3-5 предсказаний и в зависимости от того, как лягут карты судьбы (или что скажет Math.random) показывай их пользователю */
@@ -42,6 +56,6 @@ button.addEventListener('click', function() {
 
 /* Совет: заведи функцию-хелпер, которая будет заниматься только генерацией данных в диапазоне от min до max и используй ее где нужно */
 
-/* При генерации нового предсказания старое предсказание должно добавляться в начало списка «Мои предсказания» — .forecasts  */
+/* При генерации нового предсказания старое предсказание должно добавляться в начало списка «Мои предсказания» — .forecastText  */
 
 /* Для добавления предсказания в список воспользуйся шаблоном forecast-item */
